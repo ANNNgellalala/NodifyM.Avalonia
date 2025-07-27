682cb6d21df156cbb4489d19d0231446137fbbf0
ANNNgellalala <ANNNgel1619@outlook.com>
2025-07-27 23:50:50 +0800

    Add FlowChartNodeViewModel to handle flow chart node representation

---
+++ FlowChartMcpServer/ViewModels/FlowChartNodeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FlowChartMcpServer.Models;
+using NodifyM.Avalonia.ViewModelBase;
+
+namespace FlowChartMcpServer.ViewModels;
+
+public partial class FlowChartNodeViewModel : NodeViewModelBase
+{
+    public FlowChartNodeViewModel(
+        FlowChartNode node)
+    {
+        Title = node.Id;
+        Input.Add(new FlowChartConnectorViewModel(ConnectorViewModelBase.ConnectorFlow.Input, this));
+        Output.Add(new FlowChartConnectorViewModel(ConnectorViewModelBase.ConnectorFlow.Output, this));
+    }
+}
